df16c7e7cadf5e81d0fd067f0715a177185f974e
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-08-17 22:36:18 +0300

    Добавляет стартовые файлы, pnotify и basicLightbox

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+
+import { success, error, notice, info } from '@pnotify/core';
+
+const basicLightbox = require('basiclightbox')
+import * as basicLightbox from 'basiclightbox'
